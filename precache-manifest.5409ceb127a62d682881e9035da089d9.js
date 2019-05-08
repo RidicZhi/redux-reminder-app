16fdedc630a1d200874ad38092851c8c909a84b1
RidicZhi <ridiczhi@gmail.com>
2019-05-08 21:58:13 +1000

    Updates

---
+++ precache-manifest.5409ceb127a62d682881e9035da089d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f33d5ea3a8792ed4c81967c29ab620",
+    "url": "/redux-reminder-app/index.html"
+  },
+  {
+    "revision": "7ffb52772b29643465b8",
+    "url": "/redux-reminder-app/static/css/main.62d34d94.chunk.css"
+  },
+  {
+    "revision": "b960cfd99ab24d0883a8",
+    "url": "/redux-reminder-app/static/js/2.f5110606.chunk.js"
+  },
+  {
+    "revision": "7ffb52772b29643465b8",
+    "url": "/redux-reminder-app/static/js/main.935057ec.chunk.js"
+  },
+  {
+    "revision": "64e27056c1227deaba05",
+    "url": "/redux-reminder-app/static/js/runtime~main.1aba8d05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2ff37627fa495dcee63f784145fb30f.js"
+  "/redux-reminder-app/precache-manifest.5409ceb127a62d682881e9035da089d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-reminder-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
