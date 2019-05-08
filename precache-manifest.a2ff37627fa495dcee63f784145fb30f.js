c5b3c456967ba8a41b4c03cd75fe2a0bac792244
RidicZhi <ridiczhi@gmail.com>
2019-05-08 21:12:11 +1000

    Updates

---
+++ precache-manifest.a2ff37627fa495dcee63f784145fb30f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a77d55be091a033667607f32b7705e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0e4f26ce03d9bfd2b8",
+    "url": "/static/css/main.62d34d94.chunk.css"
+  },
+  {
+    "revision": "b960cfd99ab24d0883a8",
+    "url": "/static/js/2.f5110606.chunk.js"
+  },
+  {
+    "revision": "6f0e4f26ce03d9bfd2b8",
+    "url": "/static/js/main.a9991582.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
